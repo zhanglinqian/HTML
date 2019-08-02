0c94153d550f70229f5db559382554e8558d476a
zhanglinqian <51322412+zhanglinqian@users.noreply.github.com>
2019-08-02 23:04:58 +0800

    123456

--- JavaScript/task2/JavaScript/home.js
+++ JavaScript/task2/JavaScript/home.js
@@ -2,18 +2,24 @@ var  myleft = document.getElementById("left-sidebar");
     myright = document.getElementById("right-main");
    mybutton = document.getElementById("button");
    mysimple = document.getElementById("simple");
-var sidebar = "off";
+  myzhezhao = document.getElementById("zhezhao")
+//var sidebar = "off";
     mybutton.onclick = function() {
-        if (sidebar === "off") {
+        //if (sidebar === "off") {
             myleft.style.left = "0";
-            myright.style.left = "200px";
-            sidebar = "on";
-        } else if(sidebar === "on") {
-            myleft.style.left = "-200px";
-            myright.style.left = "0";
-            sidebar = "off";
-        };
-    };
+         myzhezhao.style.display = "block"
+            //myright.style.left = "200px";
+        //    sidebar = "on";
+       // } else if(sidebar === "on") {
+       //     myleft.style.left = "-200px";
+            //myright.style.left = "0";
+        //    sidebar = "off";
+       // };
+};
+    myzhezhao.onclick = function() {
+        myleft.style.left = "-220px";
+        myzhezhao.style.display = "none"
+    }
     mysimple.onclick = function() {  //页面跳转
         window.location.href = "../html/player_match.html";
     }
--- JavaScript/task2/JavaScript/player_match.js
+++ JavaScript/task2/JavaScript/player_match.js
@@ -1,85 +1,84 @@
 var myback_to_home = document.getElementById("back_to_home");     // 获取返回链接
-           mysubmi = document.getElementById("submi");            // 提交按钮
-    myback_to_home.onclick = function() {                             
-        window.location.href = "../html/home.html";}               // 返回主页
-           mysubmi.onclick = function() {                             
-            window.location.href = "../html/licensing.html";}                            //发牌
-var  myInputText = document.getElementById("input_text");       // 输入人数
-    myInputRange = document.getElementById("input_range");      // 滑动条
-           myadd = document.getElementById("add");              // 加
-      myreducing = document.getElementById("reducing");         // 减
-        mykiller = document.getElementById("killer");           // 杀手
-     mycivilians = document.getElementById("civilians");        // 平民
+           mysubmi = document.getElementById("submi");            // 提交按
+              text = document.getElementById("input_text");       // 输入人数
+             range = document.getElementById("input_range");      // 滑动条
+             myadd = document.getElementById("add");              // 加
+        myreducing = document.getElementById("reducing");         // 减
+          mykiller = document.getElementById("killer");           // 杀手
+       mycivilians = document.getElementById("civilians");        // 平民
+
 window.onload = function() {
-    myInputText.addEventListener("change",myfun1);    // 改变输入框内容  自动判断
-    myInputText.addEventListener("change", mykillers);
-   myInputRange.addEventListener("input",myfun2);     // 滑动条
-   myInputRange.addEventListener("click", mykillers);
+           text.addEventListener("change",myfun1);    // 改变输入框内容  自动判断
+          range.addEventListener("change",myfun2);    // 滑动条
           myadd.addEventListener("click",myfun3);     // 加
-          myadd.addEventListener("click",mykillers);
      myreducing.addEventListener("click",myfun4);     // 减
-     myreducing.addEventListener("click",mykillers);
+        mysubmi.addEventListener("click",mykillers);  //本地储存
+        mysubmi.addEventListener("click",stores);     //本地储存
+
+    text.addEventListener("change", mykillers);
+    range.addEventListener("change", mykillers);
+    myadd.addEventListener("click",mykillers);
+myreducing.addEventListener("click",mykillers);
 }
 function myfun1() {
-    if (isNaN(myInputText) && myInputText.value >=4 && myInputText.value <=18){
-        myInputRange.value = myInputText.value;     // 点击判断
+    if (isNaN( text ) && text.value >= 4 && text.value <= 18){
+        range.value = text.value;      // 改变输入框内容  自动判断
     } else {
-        alert("请输入玩家人数");
+        alert("请输入正确玩家人数");
     }
 }
 function myfun2() {
-    myInputText.value = myInputRange.value;     // 同步滑动条和输入框
+    text.value = range.value;          // 滑动条
 } 
 function myfun3() {
-    myInputRange.value++;
-    if(myInputText.value >= 18 ){
-        alert("请输入玩家人数");
+    range.value++;
+    if( text.value >= 18 ){
+        alert("请输入正确玩家人数");
     } else {
-        myInputText.value = myInputRange.value;  // 同步滑动条和输入框
+        text.value = range.value;     // 加
     };
 }
 function myfun4() {
-    myInputRange.value--;
-    if(myInputText.value <= 4 ){
-        alert("请输入玩家人数");
+    range.value--;
+    if( text.value <= 4 ){
+        alert("请输入正确玩家人数");
     } else {
-        myInputText.value = myInputRange.value;  // 同步滑动条和输入框
+        text.value = range.value;      // 减
     }
 }
-//console.log(typeof myInputText.value)
 function mykillers(){
-    var killer = Math.round((myInputText.value)*23/100);   //获取杀手人数
-        civilians = myInputText.value - (Math.round((myInputText.value)*23/100)); //获取平民人数
-    mykiller.innerHTML = killer;       //
- mycivilians.innerHTML =  civilians;   //
-    console.log(killer)  
-//console.log(civilians)
-    var  mynumber = [];    //人数排序
-    for (var i = 0; i < myInputRange.value; i++){
-        mynumber.push(0); //平民用数字“ 0 ” 代替
+    var killer = Math.round((text.value)*23/100);      //获取杀手人数
+        civilians = text.value - killer;               //获取平民人数
+    mykiller.innerHTML = killer;                       //输出杀手数量
+ mycivilians.innerHTML =  civilians;                   //输出平民数量
+var  mynumber = [];                                    //人数排序
+var reordering = [];                                   //重新排序
+    for (var i = 0; i < range.value; i++){
+        mynumber.push(0);                               //平民用数字“ 0 ” 代替
     };
-//console.log(mynumber) 
-    for (var a = 0; a < killer; a++) { //替换前“ N ”平民为杀手
-        mynumber[a] = 1; //杀手用数字“ 1 ” 代替
+    for (var a = 0; a < killer; a++) {                  //替换前“ N ”平民为杀手
+        mynumber[a] = 1;                                //杀手用数字“ 1 ” 代替
     }
-//console.log(mynumber)    
-    var reordering = [];         //重新排序
-for ( var b = 0; b < myInputText.value; b++ ) {
+for ( var b = 0; b < text.value; b++ ) {
     var aaa = Math.floor(Math.random()*(mynumber.length - b));
     reordering.push(mynumber[aaa]);
     mynumber[aaa] = mynumber[mynumber.length - b - 1];
 }
-
-var sss = JSON.stringify(reordering);     //转换为字符串
-    localStorage.setItem("myreordering",sss) //存入
-
-console.log(reordering)
+return reordering
+}
+    myback_to_home.onclick = function() {                             
+    window.location.href = "../html/home.html";}       // 返回主页
+function stores() {
+    var sss = JSON.stringify(mykillers());             //转换为字符串
+        localStorage.setItem("myreordering",sss)       //存入
+    window.location.href = "../html/licensing.html";   //发牌
 }
 
-//console.log(typeof myInputText.value)
+
+
 
 /*function mykillers() {
-    if(myInputText.value >= 4 && myInputText.value <= 6){
+    if(Text.value >= 4 && myInputText.value <= 6){
         mykiller.innerHTML = "1";
         mycivilians.innerHTML = myInputText.value - 1;
     } else if(myInputText.value >= 7 && myInputText.value <= 10){
